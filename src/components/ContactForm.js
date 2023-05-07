3849c59ad942da40d625d735fa9f3b9756aa774c
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-05-07 12:13:26 +0200

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,24 +1,24 @@
-import { nanoid } from "nanoid";
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import { selectContacts } from "../redux/contactsSlice";
-import css from "App.module.css"
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectContacts, addContact } from '../redux/contactsSlice';
+import css from 'App.module.css';
 
-export default function ContactForm({ onSubmit }) {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
+export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   const contacts = useSelector(selectContacts);
+  const dispatch = useDispatch();
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     const { name, value } = event.target;
 
     switch (name) {
-      case "name":
+      case 'name':
         setName(value);
         break;
 
-      case "number":
+      case 'number':
         setNumber(value);
         break;
 
@@ -27,53 +27,51 @@ export default function ContactForm({ onSubmit }) {
     }
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    const contactExists = contacts.some((contact) => contact.name === name);
+    const contactExists = contacts.some(contact => contact.name === name);
 
     if (contactExists) {
       alert(`${name} is already in contacts`);
       return;
     }
 
-    onSubmit({ id: nanoid(), name, number });
-    setName("");
-    setNumber("");
+    dispatch(addContact({ name, number }));
+    setName('');
+    setNumber('');
   };
 
   return (
     <form onSubmit={handleSubmit} className={css.form}>
-      <label>
-        Name
-      </label>
+      <label>Name</label>
       <input
-          type="text"
-          name="name"
-          className='input'
-          value={name}
-          placeholder="Enter contact's name"
-          onChange={handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+        type="text"
+        name="name"
+        className="input"
+        value={name}
+        placeholder="Enter contact's name"
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
 
-      <label>
-        Phone number
-      </label>
+      <label>Phone number</label>
       <input
-          type="tel"
-          name="number"
-          value={number}
-          placeholder="Enter contact's number"
-          onChange={handleChange}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
+        type="tel"
+        name="number"
+        value={number}
+        placeholder="Enter contact's number"
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
 
-      <button type="submit" name="submit" className={css.button}>Add contact</button>
+      <button type="submit" name="submit" className={css.button}>
+        Add contact
+      </button>
     </form>
   );
 }

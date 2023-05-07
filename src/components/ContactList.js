e72126168da4ec9c4bb1417992845718afc977d0
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-05-07 12:13:24 +0200

    Update ContactList.js

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,15 +1,26 @@
-import ContactListItem from "./ContactListElement";
-import { useSelector } from "react-redux";
-import { selectFilteredContacts } from "../redux/contactsSlice";
-import css from "../App.module.css";
+import ContactListItem from './ContactListElement';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectFilteredContacts, deleteContact } from '../redux/contactsSlice';
+import css from '../App.module.css';
 
 export default function ContactList() {
   const contacts = useSelector(selectFilteredContacts);
+  const dispatch = useDispatch();
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
 
   return (
     <ul className={css.list}>
       {contacts.map(({ id, name, number }) => (
-        <ContactListItem key={id} name={name} number={number} id={id} />
+        <ContactListItem
+          key={id}
+          name={name}
+          number={number}
+          id={id}
+          onDelete={() => handleDeleteContact(id)}
+        />
       ))}
     </ul>
   );
